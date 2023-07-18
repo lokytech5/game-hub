7d19946663d4c604238b766da6390ea0045ef3e0
lokytech <lokytech5@gmail.com>
2023-07-18 20:42:50 +0100

    change api key

--- src/components/services/api-client.ts
+++ src/components/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '0e9e7d985d7f43eb8bc72cdeafa92257'
+        key: '0e9e7d985d7f43eb8bc72cdeafa922'
     }
 })
